3589d9739b5069dd11f44649597a7bc4002ab7ac
lukit-arum <andrianaana687@gmail.com>
2025-03-11 07:30:50 +0000

    menambah script untuk req data detail dari api

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ fetch('http://www.omdbapi.com/?apikey=ac003c55&s=harry potter')
                         <div class="card-body">
                             <h5 class="card-title">${movie.Title}</h5>
                             <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
-                            <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Go somewhere</a>
+                            <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#exampleModal" data-imdbid="${movie.imdbID}">Go somewhere</a>
                         </div>
                     </div>
                 </div>`
@@ -20,4 +20,16 @@ fetch('http://www.omdbapi.com/?apikey=ac003c55&s=harry potter')
 
     const movieContainer = document.querySelector('.movie-container')
     movieContainer.innerHTML = cards
+
+    const modalDetailButton = document.querySelectorAll('.modal-detail-button')
+    modalDetailButton.forEach(btn => {
+        btn.addEventListener('click', function(){
+            const imdbid = this.dataset.imdbid
+            
+            fetch('http://www.omdbapi.com/?apikey=ac003c55&i=' + imdbid)
+             .then(response => response.json())
+             .then(detail => console.log(detail))
+
+        })
+    })
  })
