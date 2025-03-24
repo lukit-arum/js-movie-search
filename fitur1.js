3111d53553b68788685fe8519fa4360eae24816a
lukit-arum <andrianaana687@gmail.com>
2025-03-24 07:48:39 +0000

    menambah script untuk menampilkan pdata pada browser yang sudah di ambil dari api dengan fetch pada file fitur1.js

---
+++ fitur1.js
@@ -0,0 +1,36 @@
+//search button 
+const searchButton = document.querySelector('.search-button')
+searchButton.addEventListener('click', async function(){
+    const keyword = document.querySelector('.input-keyword')
+    const movies = await getMovies(keyword.value)
+    updateUi(movies)
+})
+
+function getMovies(keyword){
+    return fetch('http://www.omdbapi.com/?apikey=ac003c55&s=' + keyword)
+     .then(Response => Response.json())
+     .then(Response => Response.Search)
+}
+
+function updateUi(movies){
+    let cards = ''
+    movies.forEach(movie => {
+        cards += showMovies(movie)
+    });
+
+    const container = document.querySelector('.movie-container')
+    container.innerHTML = cards
+}
+
+function showMovies(movie){
+    return `<div class="col-md-4 my-5">
+                <div class="card">
+                    <img src="${movie.Poster}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">${movie.Title}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
+                        <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#exampleModal" data-imdbid="${movie.imdbID}">Go somewhere</a>
+                    </div>
+                </div>
+            </div>`
+}
